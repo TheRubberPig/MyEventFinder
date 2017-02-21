ffd14a3e7a0c492b393ab7c14667c0754651f2ce
David Cooper <100364693@unimail.derby.ac.uk>
2017-02-21 20:44:05 +0000

    Started search implementation

---
+++ app/src/main/java/xyz/brandonflude/developement/myeventfinderv2/SearchTeams.java
@@ -0,0 +1,58 @@
+package xyz.brandonflude.developement.myeventfinderv2;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SearchTeams extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText editTextId;
+    private Button buttonGet;
+    private TextView textViewResult;
+
+    private ProgressDialog loading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_teams);
+
+        editTextId = (EditText) findViewById(R.id.editTextId);
+        buttonGet = (Button) findViewById(R.id.buttonGet);
+        textViewResult = (TextView) findViewById(R.id.textViewResult);
+
+        buttonGet.setOnClickListener(this);
+    }
+
+    private void getData(){
+        String id = editTextId.getText().toString().trim();
+        if(id.equals("")){
+            Toast.makeText(this, "Please enter a search term", Toast.LENGTH_LONG).show();
+            return;
+        }
+        loading = ProgressDialog.show(this, "Please wait...", "Fetching...", false, false);
+
+        String url = "Add Brandons URL here";
+    }
+
+    @Override
+    public void onClick(View v){
+        getData();
+    }
+}
