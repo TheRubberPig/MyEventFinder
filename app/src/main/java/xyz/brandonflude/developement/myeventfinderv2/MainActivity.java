99cd54e4a43db785e4de2eab0914db5bd01aa871
Luiscipher <archiemolver@yahoo.co.uk>
2017-03-06 10:25:04 +0000

    Improvement on day highlighting

--- app/src/main/java/xyz/brandonflude/developement/myeventfinderv2/MainActivity.java
+++ app/src/main/java/xyz/brandonflude/developement/myeventfinderv2/MainActivity.java
@@ -3,8 +3,10 @@ package xyz.brandonflude.developement.myeventfinderv2;
 
 import android.content.Intent;
 import android.icu.util.Calendar;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CalendarView;
 import android.widget.TextView;
@@ -12,6 +14,15 @@ import android.widget.Toast;
 
 import com.squareup.timessquare.CalendarPickerView;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.HttpURLConnection;
+
+import java.net.MalformedURLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
@@ -19,17 +30,20 @@ import java.util.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity
 {
     Bundle extras;
 
+    String _UserID;
     CalendarPickerView calendar;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showRelevantDates showDates = new showRelevantDates();
         TextView user = (TextView) findViewById(R.id.show_username);
         extras = getIntent().getExtras();
         if(extras.getString("username").equals(""))
@@ -50,6 +64,26 @@ public class MainActivity extends AppCompatActivity
         //Get max date, currently 1 year forward and initalize the calendar
         calendar.init(today, nextYear.getTime())
                 .withSelectedDate(today);
+
+        try{
+            for(int i = 0; i < 28; i++)
+            {
+                if(!(showDates.execute(_UserID, String.valueOf(i)).get().equals("[]")))
+                {
+                    Log.e("ee","ee");
+                }
+            }
+        }
+        catch(InterruptedException ie)
+        {
+            ie.printStackTrace();
+        }
+        catch(ExecutionException ee)
+        {
+            ee.printStackTrace();
+        }
+
+        //calendar.highlightDates(userDates);
     }
 
     public void dateClick(View view)
@@ -67,29 +101,72 @@ public class MainActivity extends AppCompatActivity
         Intent i = new Intent(getApplicationContext(), SearchTeams.class);
         startActivity(i);
     }
+}
 
-    public void showRelevantDates()
+class showRelevantDates extends AsyncTask<String,Void,String>
+{
+    protected String doInBackground(String ...params)
     {
         List<Date> userDates = new ArrayList<>();
-
-        calendar.highlightDates(userDates);
-
-        /*for (Date currDate : userDates)
+        Date thisTime = new Date();
+        HttpURLConnection urlConnection = null;
+        String result = "";
+        String UserID = params[0];
+        String dateString = "2017-02-0";
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        try
         {
-            calendar.highlightDates();
-        }*/
-
-        String dateString = "Dec 12, 2017 9:20 PM";
-        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
-
-        try {
-            Date thisTime = format.parse(dateString);
-
+            thisTime = format.parse(dateString);
+            userDates.add(thisTime);
         }
         catch(ParseException pe)
         {
-
+            pe.printStackTrace();
         }
 
+        try
+        {
+            URL qUrl = new URL("http://calendar.brandonflude.xyz/app/services/getFixtures.php?user-id=8&date=2017-02-0");
+
+            urlConnection = (HttpURLConnection) qUrl.openConnection();
+
+            //Makes sure the server is running and accepting connections
+            int code = urlConnection.getResponseCode();
+
+            //If the server is up read the text.
+            if (code == 200) {
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                if (in != null) {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+                    String line = "";
+
+                    //While the reader is not null store in result (For us it should always only be one word)
+                    while ((line = bufferedReader.readLine()) != null)
+                        result += line;
+                }
+                //Close the input stream.
+                in.close();
+            }
+        }
+        catch (MalformedURLException mue)
+        {
+            mue.printStackTrace();
+        }
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+        return result;
     }
+
+    protected void onPostExecute(String result)
+    {
+        returnMethod(result);
+    }
+
+    private String returnMethod(String res)
+    {
+        return res;
+    }
+
 }
